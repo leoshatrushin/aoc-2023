23a6b6e1ab8435c496745daa2cbe92ed452bbbe3
leoshatrushin <leoshatrushin@gmail.com>
2023-12-09 14:04:27 +0800

    changes

--- 09/1script.py
+++ 09/1script.py
@@ -6,7 +6,7 @@ t = 0
 def n(s):
     if len(s) == 0: return 0
     d = [x - s[i - 1] for i, x in enumerate(s) if i > 0]
-    return s.pop() + n(d)
+    return s[-1] + n(d)
 
 for l in d:
     l = [int(x) for x in l.split()]
