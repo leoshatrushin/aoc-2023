a161764722e5acfea7abbe496beb75d1593cb394
leoshatrushin <leoshatrushin@gmail.com>
2023-12-09 13:33:15 +0800

    fix day9 opt

--- 09/1scriptopt.js
+++ 09/1scriptopt.js
@@ -4,12 +4,9 @@ const infile = 'input.txt';
 let t = 0;
 
 function next(s) {
-    if (s.length == 1) return 0;
-    let diffs = s.map((x, i) => {
-        if (i == 0) return 0;
-        return x - s[i - 1];
-    }).slice(1);
-    return s[s.length - 1] + next(diffs);
+    if (!s.length) return 0;
+    let d = s.map((x, i) => x - s[i - 1]).slice(1);
+    return s.pop() + next(d);
 }
 
 let d = fs.readFileSync(infile, 'utf8').trim().split('\n');
