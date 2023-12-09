541ac02a108e202457c886bae53835044742af6c
leoshatrushin <leoshatrushin@gmail.com>
2023-12-09 13:46:59 +0800

    changes

---
+++ 09/1script.py
@@ -0,0 +1,16 @@
+f = open('./input.txt', 'r')
+d = f.readlines()
+
+t = 0
+
+def n(s):
+    if len(s) == 0: return 0
+    d = [x - s[i - 1] for i, x in enumerate(s) if i > 0]
+    return s.pop() + n(d)
+
+for l in d:
+    l = [int(x) for x in l.split()]
+    t += n(l)
+
+print(t)
+f.close()
--- 09/1scriptopt.js
+++ 09/1scriptopt.js
@@ -3,16 +3,16 @@ const infile = 'input.txt';
 
 let t = 0;
 
-function next(s) {
+function n(s) {
     if (!s.length) return 0;
     let d = s.map((x, i) => x - s[i - 1]).slice(1);
-    return s.pop() + next(d);
+    return s.pop() + n(d);
 }
 
 let d = fs.readFileSync(infile, 'utf8').trim().split('\n');
 d.forEach(l => {
     l = l.split(' ').map(x => parseInt(x));
-    t += next(l);
+    t += n(l);
 })
 
 console.log(t);
